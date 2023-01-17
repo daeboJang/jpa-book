8f3d1ec2f44ec2200ab5158505898fd0b6ee1ed1
JangKwangHee <devjang2021@gmail.com>
2023-01-17 19:48:13 +0900

    Group Entity 및 실행 점검
    - Main 에 entity manager 생성 후 entity 저장 테스트

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # jpa-book
 jpa examples along with 김영한 book
+
+### 작업정리
+- gradle 로 패키지 관리하는 경우 jpa 가 entity 를 자동으로 불러들이지 못하는 경우 있음
+- jpa 설정파일 persistence.xml 에 class 태그로 직접 정의하면 됨
+
+### smartd
+- DB 설계를 다시 해보려고 함
+- 
--- src/main/java/me/devjang/Main.java
+++ src/main/java/me/devjang/Main.java
@@ -1,5 +1,35 @@
-package me.devjang;public class Main {
+package me.devjang;
+
+import me.devjang.smartd.entity.Group;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+
+            Group group = new Group();
+            group.setId(1);
+            group.setName("1st group");
+            em.persist(group);
+
+            tx.commit();
+
+        } catch (Exception e) {
+            System.out.println("oops! 에러 발생 - " + e.getMessage());
+            tx.rollback();
+        }
+        em.close();
+        emf.close();
     }
 }
